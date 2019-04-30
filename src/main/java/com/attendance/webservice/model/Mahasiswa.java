8319b92edf5f4ac3e1dbc239ff8a8f5a4047188b
fauzynurn <fnnoviansyah@gmail.com>
2019-04-30 17:39:57 +0700

    Initial commit

---
+++ src/main/java/com/attendance/webservice/model/Mahasiswa.java
@@ -0,0 +1,4 @@
+package com.attendance.webservice.model;
+
+public class Mahasiswa {
+}
---
+++ src/main/java/com/attendance/webservice/model/Sign.java
@@ -0,0 +1,22 @@
+package com.attendance.webservice.model;
+
+public class Sign {
+    private String signature;
+    private String data;
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
